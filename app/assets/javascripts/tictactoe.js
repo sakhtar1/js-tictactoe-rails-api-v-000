d6114691ddfd13df607d6e996f288b97a39c462d
Sharmina Akhtar <sakht318@gmail.com>
2018-09-26 18:01:31 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,14 +1,23 @@
 // Code your JavaScript / jQuery solution here
-const WIN_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6],
-                        [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
-var turn = 0;
-var currentGame = 0;
 
 $(document).ready(function() {
   attachListeners();
 });
 
-var player = () => turn % 2 ? 'O' : 'X';
+const WIN_COMBOS = [[0,1,2],
+                    [3,4,5],
+                    [6,7,8],
+                    [0,3,6],
+                    [1,4,7],
+                    [2,5,8],
+                    [0,4,8],
+                    [2,4,6]];
+var turn = 0;
+var currentGame = 0;
+
+var player = function(){
+  return (turn % 2 === 0) ? "X" : "O";
+};
 
 function doTurn(square) {
   updateState(square);
@@ -27,18 +36,6 @@ function resetBoard() {
   $('td').empty();
   turn = 0;
   currentGame = 0;
-}
-
-function attachListeners() {
-  $('td').on('click', function() {
-    if (!$.text(this) && !checkWinner()) {
-      doTurn(this);
-    }
-  });
-
-  $('#save').on('click', () => saveGame());
-  $('#previous').on('click', () => showPreviousGames());
-  $('#clear').on('click', () => resetBoard());
 }
 
 function checkWinner() {
@@ -133,4 +130,16 @@ function reloadGame(gameID) {
   };
 
   xhr.send(null);
+}
+
+function attachListeners() {
+  $('td').on('click', function() {
+    if (!$.text(this) && !checkWinner()) {
+      doTurn(this);
+    }
+  });
+
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
